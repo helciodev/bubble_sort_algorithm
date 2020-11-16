1c80e0f04dc0b94a79f81a32664aa39feca70233
helciodev <helcioandre@live.com>
2020-11-16 11:16:12 +0100

    adds buble_sort_by
    this algorithm sorts and array with strings

--- buble_sort.rb
+++ buble_sort.rb
@@ -10,3 +10,24 @@ def bubble_sort(arr)
     end
     arr
   end
+
+  p bubble_sort([6,4,3,1,2,3,0]) 
+
+  def bubble_sort_by (arr)
+    len = arr.length-1
+
+    len.times do 
+        arr.each_with_index do |value, index|
+            if index < len
+                sort = yield(value, arr[index + 1])
+                 if sort.positive?
+                    arr[index] = arr[index + 1]
+                    arr[index + 1] = value
+                end
+            end
+        end
+    end
+    arr
+end
+
+p bubble_sort_by(%w[dances hello hi oi]) { |left, right| left.length - right.length } 
